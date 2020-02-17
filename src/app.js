8d4f8fc4b51c98d5c0e40ca6199386bd61a18d3a
Marcos Borges <marcos.abb52@gmail.com>
2020-02-16 21:56:01 -0300

    feat(app): add initial server config

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+class App {
+  constructor () {
+    this.server = express()
+
+    this.middlewares()
+    this.routes()
+  }
+
+  middlewares () {
+    this.server.use(express.json())
+  }
+}
+
+export default new App().server
