7185b6b1dca356f3ac95fde48bbdb33293114e51
Marcos Borges <marcos.abb52@gmail.com>
2020-02-16 22:14:56 -0300

    feat(database): add database connection config

---
+++ src/database.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+class Database {
+  constructor () {
+    this.init()
+  }
+
+  init () {
+    this.connection = mongoose.connect(process.env.DATABASE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  }
+}
+
+export default new Database()
